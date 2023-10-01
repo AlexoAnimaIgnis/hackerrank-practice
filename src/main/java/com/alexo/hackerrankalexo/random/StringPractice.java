53ba3c4c6ecd9aa4b86bf44e1c7a30c0a12bdd08
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2023-10-01 14:51:17 +0800

    string practice

---
+++ src/main/java/com/alexo/hackerrankalexo/random/StringPractice.java
@@ -0,0 +1,12 @@
+package com.alexo.hackerrankalexo.random;
+
+public class StringPractice {
+    public static void main(String[] args) {
+        String test = "Certainly! String manipulation is a common topic in coding interviews, " +
+                "and there are several built-in methods in languages like Java, Python, and others that can be handy. ";
+
+        int length = test.length();
+
+        char car = test.charAt(8);
+    }
+}
