3a88f6e203da06245e12f8e0c99063f22749647e
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2023-10-20 21:57:13 +0800

    updated three sum

--- src/main/java/com/alexo/hackerrankalexo/prep/day2/TwoSum.java
+++ src/main/java/com/alexo/hackerrankalexo/prep/day2/TwoSum.java
@@ -8,23 +8,27 @@ public class TwoSum {
     //return index;
     //O(n2)
     public int[] solution(int[] arr, int target) {
-
-        for (int i = 0; i < arr.length; i++) {
+        for(int i = 0; i < arr.length; i++) {
             int diff = target - arr[i];
-
-            for (int j = i + 1; j < arr.length; j++) {
+            for(int j = i + 1; j < arr.length; j++) {
                 if(diff == arr[j]) {
-                    return new int[] {
-                            i, j
-                    };
+                    return new int[] {i, j};
                 }
             }
         }
         return null;
     }
 
+    /**
+     * int[] nums = {1, 3, 7, 9, 2};
+     * int[] ans = {3, 4};
+     * int target = 11;
+     *
+     * @param arr
+     * @param target
+     * @return
+     */
     public int[] solutionOptimized(int[] arr, int target) {
-        // store the number and the index
         Map<Integer, Integer> map = new HashMap<>();
 
         for(int i = 0; i < arr.length; i++) {
@@ -32,10 +36,10 @@ public class TwoSum {
 
             if(map.containsKey(diff)) {
                 return new int[] {
-                        map.get(diff), i
+                        map.get(diff), i // returns the index of the two
                 };
             } else {
-                // put the array element + corresponding index
+                // add the ELEMENT as key, and the INDEX as the value
                 map.put(arr[i], i);
             }
         }
